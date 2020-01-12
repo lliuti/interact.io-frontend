032134278e7a27e79f15ae75a22960ca7ca4036b
lliuti <lucas_liuti@hotmail.com>
2020-01-12 00:06:40 -0300

    Adding FEED to routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import SignIn from './Pages/Account/SignIn/index';
 import SignUp from './Pages/Account/SignUp/index';
+import Feed from './Pages/Feed/index';
 
 export default function Routes() {
   return (
@@ -10,6 +11,7 @@ export default function Routes() {
       <Switch>
         <Route path='/' exact component={SignIn}/>
         <Route path='/register' component={SignUp}/>
+        <Route path='/feed' component={Feed}/>
       </Switch>
     </BrowserRouter>
   )
